9bd5f58ee55f03d94c3b12687531cc8cfe6a6965
Seth DImmer <sethdimmer1@gmail.com>
2022-08-23 16:07:55 -0400

    initial commit

--- app.js
+++ app.js
@@ -8,9 +8,20 @@ fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => {
     //  console.log(response.json())
     response.json().then(data => {
         console.log(data)
-        emailRef.innerHTML = data.name;
+        emailRef.innerHTML = data.email;
     })
 })
+
+// New Cleaner way of what is above starts at
+// 9:10
+fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => {
+    return response.json()
+})
+.then(data => {
+    console.log(data)
+});
+
+
 
 // 2. Async/Await
 
