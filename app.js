aafa693ebc3e1459705de38fe6bde072f3e7e9c4
Seth DImmer <sethdimmer1@gmail.com>
2022-08-23 14:18:38 -0400

    initial commit

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
 
 // 1. Then
 
-
+fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => {
+     console.log(response.json())
+})
 
 // 2. Async/Await
+
+
