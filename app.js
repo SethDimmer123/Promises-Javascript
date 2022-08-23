4dc28f63c800b0b07138ed2f31fcc2a1351257eb
Seth DImmer <sethdimmer1@gmail.com>
2022-08-23 15:28:00 -0400

    initial commit

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => {
     //  console.log(response.json())
     response.json().then(data => {
         console.log(data)
+        emailRef.innerHTML = data.name;
     })
 })
 
