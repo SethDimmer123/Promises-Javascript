7682f37e676ebb187ed7fe24bb735d227c949e63
Seth DImmer <sethdimmer1@gmail.com>
2022-08-23 16:17:27 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 // console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
-const emailRef = document.querySelector(".email");
-console.log
+// const emailRef = document.querySelector(".email");
+// console.log
 
 // 1. Then
 
-fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => {
+// fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => {
     //  console.log(response.json())
-    response.json().then(data => {
-        console.log(data)
-        emailRef.innerHTML = data.email;
-    })
-})
+    // response.json().then(data => {
+        // console.log(data)
+        // emailRef.innerHTML = data.email;
+    // })
+// })
 
 // New Cleaner way of what is above starts at
 // 9:10
-fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => {
-    return response.json()
-})
-.then(data => {
-    console.log(data)
-});
+// fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => {
+    // return response.json()
+// })
+// .then(data => {
+    // console.log(data)
+// });
 
 
 
