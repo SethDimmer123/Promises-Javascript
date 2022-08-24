8a6f87d784cf445d0df63c00832581121ec70b2f
Seth DImmer <sethdimmer1@gmail.com>
2022-08-24 10:09:06 -0400

    initial commit

--- app.js
+++ app.js
@@ -25,9 +25,12 @@
 
 // 2. Async/Await
 async function main() {
+console.log(1)
 const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
-await response.json()
-console.log(response.json())
+console.log(2)
+ const data = await response.json()
+console.log(3)
+emailRef.innerHTML = data.email
 } 
 
 main()
